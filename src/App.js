e88026ec5d4476afe3bcc0a8753277ae6d84ae50
Anonymous21-03 <160504530+Anonymous21-03@users.noreply.github.com>
2024-12-30 14:15:32 +0530

    Deployment Ready

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
-import Login from './Components/Login'
-import Dashboard from './Components/Dashboard'
-import Register from './Components/Register'
-import Tasks from './Components/Tasks'
+  import React from 'react'
+  import { HashRouter  as Router, Route, Routes, Navigate } from 'react-router-dom'
+  import Login from './Components/Login'
+  import Dashboard from './Components/Dashboard'
+  import Register from './Components/Register'
+  import Tasks from './Components/Tasks'
 
-const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem('token')
-  if (!token) {
-    return <Navigate to="/login" />
+  const ProtectedRoute = ({ children }) => {
+    const token = localStorage.getItem('token')
+    if (!token) {
+      return <Navigate to="/login" />
+    }
+    return children
   }
-  return children
-}
 
-function App () {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/dashboard' element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        } />
-        <Route path='/tasks' element={
-          <ProtectedRoute>
-            <Tasks />
-          </ProtectedRoute>
-        } />
-        <Route path='/' element={<Navigate to="/login" />} />
-      </Routes>
-    </Router>
-  )
-}
-
-export default App
+  function App () {
+    return (
+      <Router>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/dashboard' element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+          <Route path='/tasks' element={
+            <ProtectedRoute>
+              <Tasks />
+            </ProtectedRoute>
+          } />
+          <Route path='/' element={<Navigate to="/login" />} />
+        </Routes>
+      </Router>
+    )
+  }
+  
+  export default App
